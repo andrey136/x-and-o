9fcb9563ea358dcfa9c62120d89b027efd2795fb
andrey_mardash <andrey.mardash@gmail.com>
2019-08-26 12:05:51 +0300

    third commit solved o problem

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function chosen(id){
     if(!chosenItems_X.includes(id) && !chosenItems_O.includes(id)){
         count++;
         const x = document.createElement('img');
-        count % 2 ? x.src = 'photos/images.png' : x.src = 'photos/fileicon-red-xpng-x-png-256_256.png';
+        count % 2 ? x.src = 'photos/o-png-89-images-in-collection-page-2-png-o-2000_2000.png' : x.src = 'photos/fileicon-red-xpng-x-png-256_256.png';
         x.width = 120;
         x.height = 120;
         document.getElementById(id).appendChild(x);
